3b0b794455995d76f62e9c9132832078c5546bbf
Gustavo <villegasgustavoopel@hotmail.com>
2022-05-19 15:17:47 -0300

    subir proyecto

---
+++ cursos.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Cursos</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- bootstrap cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/cursos.css">
+
+</head>
+
+<body>
+
+   <!-- header section starts  -->
+
+   <header class="header fixed-top">
+
+      <div class="container curso">
+
+         <div class="row align-items-center justify-content-between">
+
+            <img src="images/logo2.png" class="logo2" alt="">
+            <a href="#home" class="logo">Abinadab<span>Nails.</span></a>
+
+            <nav class="nav">
+               <a href="/home">Home</a>
+               <a href="#">Cursos</a>
+            </nav>
+
+            <a href="http://mobooker.co/abinadab-unas" class="link-btn">Concertar Cita</a>
+
+            <div id="menu-btn" class="fas fa-bars"></div>
+
+         </div>
+
+      </div>
+ 
+   </header>
+
+   <!-- header section ends -->
+   <section class="about" id="about">
+      <div class="container">
+         <div class="row align-items-center">
+            <div class="col-md-6 image">
+               <img src="images/Nails7.jpeg" class="w-100 mb-5 mb-md-0" alt="">
+            </div>
+
+            <div class="col-md-6 content">
+               <span>Nosotros</span>
+               <h3>Cuidamos de cada detalle para ti</h3>
+               <p>Nuestra formacion Profesional hace que cada detalle valga, el cuidado de tus manos es nuestra principal 
+                  prioridad y hacerte sentir bien nuestro compromiso. En Abinandab nunca saldras igual.</p>
+            
+            </div>
+         </div>
+      </div>
+      <div class="container">
+         <div class="row align-items-center">
+            <div class="col-md-6 image">
+               <img src="images/Nails7.jpeg" class="w-100 mb-5 mb-md-0" alt="">
+            </div>
+
+            <div class="col-md-6 content">
+               <span>Nosotros</span>
+               <h3>Cuidamos de cada detalle para ti</h3>
+               <p>Nuestra formacion Profesional hace que cada detalle valga, el cuidado de tus manos es nuestra principal 
+                  prioridad y hacerte sentir bien nuestro compromiso. En Abinandab nunca saldras igual.</p>
+            
+            </div>
+         </div>
+      </div>
+      <div class="container">
+         <div class="row align-items-center">
+            <div class="col-md-6 image">
+               <img src="images/Nails7.jpeg" class="w-100 mb-5 mb-md-0" alt="">
+            </div>
+
+            <div class="col-md-6 content">
+               <span>Nosotros</span>
+               <h3>Cuidamos de cada detalle para ti</h3>
+               <p>Nuestra formacion Profesional hace que cada detalle valga, el cuidado de tus manos es nuestra principal 
+                  prioridad y hacerte sentir bien nuestro compromiso. En Abinandab nunca saldras igual.</p>
+            
+            </div>
+         </div>
+      </div>
+   </section>
+
+   <!-- home section starts  -->
+   
+   <!-- footer section starts  -->
+
+   <section class="footer">
+      <div class="box-container container">
+         <div class="box">
+            <i class="fas fa-phone"></i>
+            <h3>Numero de Telefono</h3>
+            <p>(+58)911-55046143</p>
+         </div>
+
+         <div class="box">
+            <i class="fas fa-map-marker-alt"></i>
+            <h3>Nuestra Dirección</h3>
+            <p>Ciudad de la Paz</p>
+         </div>
+
+         <div class="box">
+            <i class="fas fa-clock"></i>
+            <h3>Horarios</h3>
+            <p>09:00am to 17:00pm</p>
+         </div>
+
+         <div class="box">
+            <i class="fas fa-envelope"></i>
+            <h3>email</h3>
+            <p>AbinadabNails@hotmail.com</p>
+            <
+         </div>
+      </div>
+      <div class="credit"> &copy; copyright @ <?php echo date('Y'); ?> by <span>Gustavo Villegas Frontend Developer</span> </div>
+   </section>
+
+   <!-- footer section ends -->
+
+   <!-- custom js file link  -->
+   <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+   <script src="js/script.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>AbinadabNails</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- bootstrap cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+   <!-- header section starts  -->
+
+   <header class="header fixed-top">
+
+      <div class="container">
+
+         <div class="row align-items-center justify-content-between">
+
+            <img src="images/logo2.png" class="logo2" alt="">
+            <a href="#home" class="logo">Abinadab<span>Nails.</span></a>
+
+            <nav class="nav">
+               <a href="#home">Home</a>
+               <a href="#about">Nosotros</a>
+               <a href="#services">Servicios</a>
+               <a href="#reviews">Opiniones</a>
+               <a href="#contact">Contacto</a>
+               <a href="/cursos.php">Cursos</a>
+            </nav>
+
+            <a href="http://mobooker.co/abinadab-unas" class="link-btn">Concertar Cita</a>
+
+            <div id="menu-btn" class="fas fa-bars"></div>
+
+         </div>
+
+      </div>
+
+   </header>
+
+   <!-- header section ends -->
+
+   <!-- home section starts  -->
+
+   <section class="home" id="home">
+
+      <div class="container">
+
+         <div class="row min-vh-100 align-items-center">
+            <div class="content text-center text-md-left">
+               <h3>Dejanos Embellecer tus Manos</h3>
+               <p>En nuestro Salon amamos lo que hacemos y por eso te hacemos sentir como lo mereces, siempre saldras renonava.</p>
+               <a href="http://mobooker.co/abinadab-unas" class="link-btn">Concertar Cita</a>
+            </div>
+         </div>
+
+      </div>
+
+   </section>
+
+   <!-- home section ends -->
+
+   <!-- about section starts  -->
+
+   <section class="about" id="about">
+      <div class="container">
+         <div class="row align-items-center">
+            <div class="col-md-6 image">
+               <img src="images/Nails7.jpeg" class="w-100 mb-5 mb-md-0" alt="">
+            </div>
+
+            <div class="col-md-6 content">
+               <span>Nosotros</span>
+               <h3>Cuidamos de cada detalle para ti</h3>
+               <p>Nuestra formacion Profesional hace que cada detalle valga, el cuidado de tus manos es nuestra principal 
+                  prioridad y hacerte sentir bien nuestro compromiso. En Abinandab nunca saldras igual.</p>
+               <a href="http://mobooker.co/abinadab-unas" class="link-btn">Concertar Cita</a>
+            </div>
+         </div>
+      </div>
+   </section>
+
+   <!-- about section ends -->
+
+   <!-- services section starts  -->
+
+   <section class="services" id="services">
+      <h1 class="heading">Nuestros Servicios</h1>
+        <div class="box-container container">
+
+           <div class="box">
+              <img src="images/amor.gif" alt="">
+                 <h3>Manicura</h3>
+                   <p>Una manicura es un tratamiento de belleza cosmético para las uñas y manos que suele realizarse en casa o en un 
+                      salón de belleza. En una manicura se cortan o liman los bordes de las uñas, se realizan masajes a las manos y se 
+                      aplica esmalte de uñas.
+                   </p>
+           </div>
+
+           <div class="box">
+              <img src="images/bendecir.gif" alt="">
+                 <h3>Esculpido</h3>
+                  <p>Las uñas esculpidas en gel son extensiones artificiales de las uñas realizadas con geles de construcción, utilizando 
+                     moldes para dar la forma y extensión deseada. Como la palabra lo indica, lo que se hace es “esculpir” una uña artificial 
+                     sobre la uña natural, fijándola con luz ultravioleta.
+                  </p>
+          </div>
+
+          <div class="box">
+              <img src="images/escritorio-del-maestro.gif" alt="">
+                 <h3>Kapping</h3>
+                  <p>El Kapping consiste en aplicar una fina capa de acrílico o gel fortificador sobre la uña que actuará como una barrera
+                     protectora. A diferencia de las uñas esculpidas, este baño en gel kapping no alarga la uña natural sino que acompaña el
+                     crecimiento de la misma y dura hasta 20 días.
+                  </p>
+          </div>
+
+          <div class="box">
+             <img src="images/karate.gif" alt="">
+                <h3>Pedicura</h3>
+                 <p>Un auténtico tratamiento de lujo de entre 45 y 60 minutos para manos y pies que incluye exfoliación e hidratación, cutículas,
+                    masaje, limado y esmaltado. Todo ello, acompañado de música relajante, un espacio cómodo y una cuidada selección de té.
+                  </p>
+          </div>
+
+          <div class="box">
+             <img src="images/lampara-de-mesa.gif" alt="">
+                <h3>Reconstruccion</h3>
+                 <p>La reconstrucción es una técnica  que consiste en la aplicación y colocación de una uña de gel, encima de la uña que tenemos 
+                    lesionada. Con el fin de simular la presencia de una uña estéticamente normal en ese dedo. A parte del objetivo meramente 
+                    estético que pueda presentar esta técnica
+                  </p>
+          </div>
+
+          <div class="box">
+             <img src="images/mudra.gif" alt="">
+               <h3>Esmaltado Semi-Permanente</h3>
+                <p>es un sistema de duración corta, entorno 2-3 semanas, sin hacer extensión de la uña, es decir, no se puede alargar la uña 
+                 con este tipo de esmalte. Para eliminarlo, basta con un removedor (acetona). Además, lo puedes encontrar en diferentes colores,
+                 ya que es un híbrido de esmalte y gel.
+               </p>
+          </div>
+      </div>
+   </section>
+
+   <!-- services section ends -->
+
+   <!-- process section starts  --> 
+
+   <section class="process">
+      <h1 class="heading">Mis Trabajos</h1>
+        <div class="box-container container">
+             <div class="box">
+                 <iframe src="https://snapwidget.com/embed/987865" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" ></iframe>        
+             </div>
+
+             <div class="box">
+                <!-- SnapWidget -->
+                 <iframe src="https://snapwidget.com/embed/987870" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" ></iframe>
+            </div>
+
+            <div class="box">
+                 <iframe src="https://snapwidget.com/embed/987872" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" ></iframe>
+            </div>
+         </div>
+   </section>
+
+   <!-- process section ends -->
+
+   <!-- reviews section starts  -->
+
+   <section class="reviews" id="reviews">
+      <h1 class="heading"> Opiniones </h1>
+           <div class="box-container container">
+              <div class="elfsight-app-7a95869d-41f3-40ea-a71a-2c7f5bf82aba"></div>
+           </div>
+   </section>
+
+   <!-- reviews section ends -->
+  <section class="ubicacion">
+      <div class="mapa"> 
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1642.9285826103255!2d-58.463208142030204!3d-34.55717169511788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5053bdfc0c3%3A0xe2016de45557d7d2!2sAbinadab!5e0!3m2!1ses!2sar!4v1646606883705!5m2!1ses!2sar" class="google"></iframe>
+     </div>
+  </section>
+   <!-- contact section starts  -->
+
+   <section class="contact" id="contact">
+       <h1 class="heading">Concertar Cita</h1>
+         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <?php
+            if (isset($message)) {
+            foreach ($message as $message) {
+               echo '<p class="message">' . $message . '</p>';
+            }
+              }
+            ?>
+            <span>Nombre :</span>
+              <input type="text" name="name" placeholder="enter your name" class="box" required>
+            <span>email :</span>
+              <input type="email" name="email" placeholder="enter your email" class="box" required>
+            <span>Numero :</span>
+              <input type="number" name="number" placeholder="enter your number" class="box" required>
+            <span>Dia de la cita :</span>
+              <input type="datetime-local" name="date" class="box" required>
+                <input type="submit" value="make appointment" name="submit" class="link-btn">
+       </form>
+   </section>
+
+   <!-- contact section ends -->
+
+   <!-- footer section starts  -->
+
+   <section class="footer">
+      <div class="box-container container">
+         <div class="box">
+            <i class="fas fa-phone"></i>
+            <h3>Numero de Telefono</h3>
+            <p>(+58)911-55046143</p>
+         </div>
+
+         <div class="box">
+            <i class="fas fa-map-marker-alt"></i>
+            <h3>Nuestra Dirección</h3>
+            <p>Ciudad de la Paz</p>
+         </div>
+
+         <div class="box">
+            <i class="fas fa-clock"></i>
+            <h3>Horarios</h3>
+            <p>09:00am to 17:00pm</p>
+         </div>
+
+         <div class="box">
+            <i class="fas fa-envelope"></i>
+            <h3>email</h3>
+            <p>AbinadabNails@hotmail.com</p>
+            <
+         </div>
+      </div>
+      <div class="credit"> &copy; copyright @ <?php echo date('Y'); ?> by <span>Gustavo Villegas Frontend Developer</span> </div>
+   </section>
+
+   <!-- footer section ends -->
+
+   <!-- custom js file link  -->
+   <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+   <script src="js/script.js"></script>
+
+</body>
+
+</html>
